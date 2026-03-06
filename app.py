f2ab87c8881397de15a55e9b5d7566e2c275f95d
nivii205 <nivedithapr2005@gmail.com>
2026-03-06 15:53:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -98,10 +98,11 @@ def complete(id):
 
     return redirect("/")
 
- import os
+import os
 
- init_db()
-
- if __name__ == "__main__":
+init_db()
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
